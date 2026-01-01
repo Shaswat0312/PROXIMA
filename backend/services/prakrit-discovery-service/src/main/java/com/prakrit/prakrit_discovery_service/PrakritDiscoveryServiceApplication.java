ee49807639084c88d7bbf94b60c5cf34f4a6e231
aastha-sin-09 <136198084+aastha-sin-09@users.noreply.github.com>
2026-01-01 17:14:18 +0530

    tech: initialize core infrastructure spring boot services

---
+++ backend/services/prakrit-discovery-service/src/main/java/com/prakrit/prakrit_discovery_service/PrakritDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prakrit.prakrit_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakritDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrakritDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/services/prakrit-discovery-service/src/test/java/com/prakrit/prakrit_discovery_service/PrakritDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakrit.prakrit_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakritDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
